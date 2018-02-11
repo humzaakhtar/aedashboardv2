4834056e6b3a3f296f8c66adea81467dafb19cf4
Humza Akhtar <humza@Humzas-MacBook-Pro.local>
2018-02-12 01:04:07 +0800

    humzaedit

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
   var timeData = [],
-    temperatureData = [],
-    humidityData = [];
+    pressureData = [],
+    flowrateData = [];
   var data = {
     labels: timeData,
     datasets: [
       {
         fill: false,
-        label: 'Temperature',
-        yAxisID: 'Temperature',
+        label: 'Pressure',
+        yAxisID: 'Pressure',
         borderColor: "rgba(255, 204, 0, 1)",
         pointBoarderColor: "rgba(255, 204, 0, 1)",
         backgroundColor: "rgba(255, 204, 0, 0.4)",
         pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
         pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-        data: temperatureData
+        data: pressureData
       },
       {
         fill: false,
-        label: 'Humidity',
-        yAxisID: 'Humidity',
+        label: 'Flowrate',
+        yAxisID: 'Flowrate',
         borderColor: "rgba(24, 120, 240, 1)",
         pointBoarderColor: "rgba(24, 120, 240, 1)",
         backgroundColor: "rgba(24, 120, 240, 0.4)",
         pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
         pointHoverBorderColor: "rgba(24, 120, 240, 1)",
-        data: humidityData
+        data: flowrateData
       }
     ]
   }
@@ -33,23 +33,23 @@ $(document).ready(function () {
   var basicOption = {
     title: {
       display: true,
-      text: 'Temperature & Humidity Real-time Data',
+      text: 'Pressure & Flow Rate Real-time Data',
       fontSize: 36
     },
     scales: {
       yAxes: [{
-        id: 'Temperature',
+        id: 'Pressure',
         type: 'linear',
         scaleLabel: {
-          labelString: 'Temperature(C)',
+          labelString: 'Pascal',
           display: true
         },
         position: 'left',
       }, {
-          id: 'Humidity',
+          id: 'Flowrate',
           type: 'linear',
           scaleLabel: {
-            labelString: 'Humidity(%)',
+            labelString: 'm^3/s',
             display: true
           },
           position: 'right'
@@ -74,24 +74,24 @@ $(document).ready(function () {
     console.log('receive message' + message.data);
     try {
       var obj = JSON.parse(message.data);
-      if(!obj.time || !obj.temperature) {
+      if(!obj.time || !obj.pressure) {
         return;
       }
       timeData.push(obj.time);
-      temperatureData.push(obj.temperature);
+      pressureData.push(obj.pressure);
       // only keep no more than 50 points in the line chart
       const maxLen = 50;
       var len = timeData.length;
       if (len > maxLen) {
         timeData.shift();
-        temperatureData.shift();
+        pressureData.shift();
       }
 
-      if (obj.humidity) {
-        humidityData.push(obj.humidity);
+      if (obj.flowrate) {
+        flowrateData.push(obj.flowrate);
       }
-      if (humidityData.length > maxLen) {
-        humidityData.shift();
+      if (flowrateData.length > maxLen) {
+        flowrateData.shift();
       }
 
       myLineChart.update();
