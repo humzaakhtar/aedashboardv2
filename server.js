5cfdcbb94474873f0e497c07e7ddab2652a0daff
Humza Akhtar <humza@Humzas-MacBook-Pro.local>
2018-04-23 11:14:38 +0800

    separating web socket messages at server side14

--- server.js
+++ server.js
@@ -57,8 +57,8 @@ wss.on('connection', function connection(ws) {
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
         try {
-          console.log('sending data ' + msg);
-          client.send(msg);
+          console.log('sending data ');
+          client.send('sending data');
         } catch (e) {
           console.error(e);
         }
