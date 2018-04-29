9b08ad6d625d923c312d0bb2f699372d4ecc927f
Humza Akhtar <humza@Humzas-MacBook-Pro.local>
2018-04-29 16:12:27 +0800

    testing ws with res.send1

--- server.js
+++ server.js
@@ -41,6 +41,79 @@ function isJson(str) {
 }
 
 
+router.get('/', function(req, res) {
+
+
+  const server = http.createServer(app);
+  const wss = new WebSocket.Server({
+    server
+  });
+
+
+  // Broadcast to all.
+  wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        try {
+          //console.log('sending data ' + data);
+          client.send(data);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    });
+  };
+
+
+
+
+
+  var iotHubReader = new iotHubClient(process.env['Azure.IoT.IoTHub.ConnectionString'], process.env['Azure.IoT.IoTHub.ConsumerGroup']);
+  iotHubReader.startReadMessage(function(obj, date) {
+    try {
+      console.log(date);
+      date = date || Date.now();
+      var date = moment.utc(date).format('YYYY-MM-DD HH:mm:ss');
+      var stillUtc = moment.utc(date).toDate();
+      var local = moment(stillUtc).local().format('hh:mm:ss');
+      wss.broadcast(JSON.stringify(Object.assign(obj, {
+        time: local
+      })));
+    } catch (err) {
+      console.log(obj);
+      console.error(err);
+    }
+  });
+
+
+
+  var port = normalizePort(process.env.PORT || '3001');
+  server.listen(port, function listening() {
+    console.log('Listening on %d', server.address().port);
+  });
+
+  /**
+   * Normalize a port into a number, string, or false.
+   */
+
+  function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+
+    return false;
+  }
+
+});
+
 
 
 router.get('/download', function(req, res) {
@@ -103,76 +176,3 @@ router.post('/visdata', function(req, res) {
     res.send("error");
   }
 });
-
-
-
-
-
-
-const server = http.createServer(app);
-const wss = new WebSocket.Server({
-  server
-});
-
-
-// Broadcast to all.
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      try {
-        //console.log('sending data ' + data);
-        client.send(data);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  });
-};
-
-
-
-
-
-var iotHubReader = new iotHubClient(process.env['Azure.IoT.IoTHub.ConnectionString'], process.env['Azure.IoT.IoTHub.ConsumerGroup']);
-iotHubReader.startReadMessage(function(obj, date) {
-  try {
-    console.log(date);
-    date = date || Date.now();
-    var date = moment.utc(date).format('YYYY-MM-DD HH:mm:ss');
-    var stillUtc = moment.utc(date).toDate();
-    var local = moment(stillUtc).local().format('hh:mm:ss');
-    wss.broadcast(JSON.stringify(Object.assign(obj, {
-      time: local
-    })));
-  } catch (err) {
-    console.log(obj);
-    console.error(err);
-  }
-});
-
-
-
-var port = normalizePort(process.env.PORT || '3001');
-server.listen(port, function listening() {
-  console.log('Listening on %d', server.address().port);
-});
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
