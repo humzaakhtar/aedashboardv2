a15d9209a344cd81fa24f8c596f5bfc05cc83970
Humza Akhtar <humza@Humzas-MacBook-Pro.local>
2018-04-29 18:39:13 +0800

    testing ws with res.send4

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ function isJson(str) {
   });
 
 
-var iotHubReader = new iotHubClient(process.env['Azure.IoT.IoTHub.ConnectionString'], process.env['Azure.IoT.IoTHub.ConsumerGroup']);
+iotHubReader = new iotHubClient(process.env['Azure.IoT.IoTHub.ConnectionString'], process.env['Azure.IoT.IoTHub.ConsumerGroup']);
 
 
 iotHubReader.startReadMessage(function(obj, date) {
@@ -170,6 +170,30 @@ router.post('/visdata', function(req, res) {
             console.log("all rows downloaded")
             fs.writeFileSync('sensordata.txt', JSON.stringify(jsonArray));
             res.send("file downloaded");
+
+            iotHubReader = new iotHubClient(process.env['Azure.IoT.IoTHub.ConnectionString'], process.env['Azure.IoT.IoTHub.ConsumerGroup']);
+
+
+            iotHubReader.startReadMessage(function(obj, date) {
+                try {
+                  console.log(date);
+                  date = date || Date.now();
+                  var date = moment.utc(date).format('YYYY-MM-DD HH:mm:ss');
+                  var stillUtc = moment.utc(date).toDate();
+                  var local = moment(stillUtc).local().format('hh:mm:ss');
+                  wss.broadcast(JSON.stringify(Object.assign(obj, {
+                    time: local
+                  })));
+                } catch (err) {
+                  console.log(obj);
+                  console.error(err);
+                }
+              });
+
+
+
+
+
           });
           connectionsql.execSql(request);
         }
